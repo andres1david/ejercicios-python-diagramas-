51d4eef0500bee7fce5b815bd23cad8a3cea186d
DavidLagos <davidlagos302@gmail.com>
2023-04-25 16:35:19 -0500

    subiendo los diagramas 123

--- diagrama3.py
+++ diagrama3.py
@@ -9,8 +9,26 @@ while True:
         prendercomputador = input ("si")
         if prendercomputador == "1":
             print("entrar a google")
-
-        break
-    else:
-        print(f"fin {2}")
-
+            entrargoogle = input("si=")
+            if entrargoogle == "1":
+                print("buescar retaurante")
+                buscarrestaurante = input ("si=")
+                if buscarrestaurante == "1":
+                    print("entrar pagina wed")
+                    entrarpagina = input("si=")
+                    if entrarpagina == "1":
+                        print("hay disponibilidad")
+                        haydisponibilidad = ("si o no=")
+                        if haydisponibilidad == "1":
+                            print("hay mesa para dos")
+                            haymesa = input("si o no=")
+                            if haymesa == "1":
+                                print ("hacer reserba")
+                                ("fin")
+                                break
+                            elif haymesa == "2":
+                                print("buscar otro restaurante")
+                        elif haydisponibilidad == "2":
+                            print("buscar otro restaurante")
+    elif quieresiraunrestaurante == "2":
+        print ("fin")
